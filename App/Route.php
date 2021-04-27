987bf8bc642735fa3a3190e4b2ba4c361a551336
JLucasMelo <jlmaia100@gmail.com>
2021-04-27 16:55:23 -0300

    Update -  .gitignore in empty folders

--- App/Route.php
+++ App/Route.php
@@ -2,28 +2,12 @@
 
 namespace App;
 
-class Route
+use MF\Init\Bootstrap;
+
+class Route extends Bootstrap
 {
 
-    private $routes;
-
-    public function __construct()
-    {
-        $this->initRoutes();
-        $this->run($this->getUrl());
-    }
-
-    public function getRoutes()
-    {
-        return $this->routes;
-    }
-
-    public function setRoutes(array $routes)
-    {
-        $this->routes = $routes;
-    }
-
-    public function initRoutes()
+    protected function initRoutes()
     {
 
         $routes['home'] = array(
@@ -39,26 +23,5 @@ class Route
         );
 
         $this->setRoutes($routes);
-    }
-
-    public function run($url)
-    {
-        foreach ($this->getRoutes() as $key => $route) {
-
-            if ($url == $route['route']) {
-                $class = "App\\Controllers\\" . ucfirst($route['controller']);
-
-                $controller = new $class;
-
-                $action = $route['action'];
-
-                $controller->$action();
-            }
-        }
-    }
-
-    public function getUrl()
-    {
-        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     }
 }
