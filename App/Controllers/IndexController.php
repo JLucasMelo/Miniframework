757ec2bff1fadff5a9535dca936e8630f240e880
JLucasMelo <jlmaia100@gmail.com>
2021-04-27 17:50:42 -0300

    Fourth Commit

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -4,13 +4,28 @@ namespace App\Controllers;
 
 class IndexController
 {
+    private  $view;
+
+    public function __construct() {
+        $this->view = new \stdClass();
+    }
+
     public function index()
     {
-        echo 'Chegamos ao indexController e disparamos a action index';
+        $this->render('index');
     }
 
     public function sobreNos()
     {
-        echo 'Chegamos ao indexController e disparamos a action sobre nós';
+        $this->render('sobreNos');
+    }
+
+    public function render($view)
+    {
+        $classAtual = get_class($this);
+        $classSemPath = str_replace('App\\Controllers\\', '', $classAtual);
+        $class = str_replace('Controller', '', $classSemPath);
+
+        require_once "../App/Views/" . $class . "/" . $view . ".phtml";
     }
 }
